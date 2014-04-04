a28e1aa9bfdb885c6145bb6d6f39753c4bc416ab
Juan Carlos Moreno <jcrogel@gmail.com>
2014-04-03 18:28:40 -0700

    Initial Commit

---
+++ Mango/MangoAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MangoAppDelegate.m
+//  Mango
+//
+//  Created by Juan Carlos Moreno on 4/3/14.
+//  Copyright (c) 2014 Juan Carlos Moreno. All rights reserved.
+//
+
+#import "MangoAppDelegate.h"
+
+@implementation MangoAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MangoTests/MangoTests.m
@@ -0,0 +1,34 @@
+//
+//  MangoTests.m
+//  MangoTests
+//
+//  Created by Juan Carlos Moreno on 4/3/14.
+//  Copyright (c) 2014 Juan Carlos Moreno. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MangoTests : XCTestCase
+
+@end
+
+@implementation MangoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
